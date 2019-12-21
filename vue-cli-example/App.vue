002145a905da3fe4bedb0c17f71e022b0a71757f
messiahhh <616348105@qq.com>
2019-12-21 19:26:41 +0800

    fixed

---
+++ vue-cli-example/App.vue
@@ -0,0 +1,3 @@
+<template lang="html">
+    <h1>hello</h1>
+</template>
--- webpack/src/main.js
+++
@@ -1,5 +0,0 @@
-import './test.css'
-import img from './test.jpg'
-let image = new Image()
-image.src = img
-document.body.appendChild(image)
